09abdbb8f0b5c2bd11a24977142f8265f83b0f8c
mariam-adepoju <marrizon.am@gmail.com>
2025-06-19 20:20:41 +0100

    feature-appointment page

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ function showMenu() {
     menuClose.style.display = "block"
 }
 menuOpen.addEventListener("click", showMenu)
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+    grabCursor: true,
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //     dynamicBullets: true,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
